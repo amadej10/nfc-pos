251e35ef7a25c611be9e1548f17c07c9fdb21c4f
Amadej Skornšek <skornsek.a@gmail.com>
2022-07-02 17:28:29 +0200

    Updated identity db

--- src/Infrastructure/Persistence/Migrations/20220702151716_AddUserData.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20220702151716_AddUserData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using nfc_pos.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using nfc_pos.Infrastructure.Persistence;
 namespace nfc_pos.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220702151716_AddUserData")]
+    partial class AddUserData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Persistence/Migrations/20220702151716_AddUserData.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nfc_pos.Infrastructure.Persistence.Migrations
+{
+    public partial class AddUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NfcId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_NfcId",
+                table: "AspNetUsers",
+                column: "NfcId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_NfcId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NfcId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "AspNetUsers");
+        }
+    }
+}
